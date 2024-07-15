5ab52908f7cd93aec5719602ddcc0fcac89d14f6
mi-ael <miael.oss.1970@gmail.com>
2024-07-15 21:55:23 +0200

    cleanup main

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,7 @@ const std = @import("std");
 
 pub const day1 = @import("01.zig");
 
-fn get2() !struct { u32, u32 } {
-    return .{ 1, 2 };
-}
-
-pub fn main() !void {
-    const a, const b = try get2();
-    std.debug.print("{}, {}\n", .{ a, b });
-}
+pub fn main() !void {}
 
 test {
     std.testing.refAllDecls(@This());
